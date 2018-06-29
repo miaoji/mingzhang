48be49d646084c6998bba4eda53349bb56b3c579
winnerwly <975080391@qq.com>
2018-06-29 12:04:27 +0800

    ..

--- src/App.vue
+++ src/App.vue
@@ -7,57 +7,60 @@
 </template>
 
 <script>
-  import {gettoken as getToken, storage} from '@/utils'
-  import {guid} from '@/utils/uuid'
+import { gettoken as getToken, storage } from '@/utils'
+import { guid } from '@/utils/uuid'
 
-  export default {
-    name: 'app',
-    data () {
-      return {
-        wid: 'width:1024px'
-      }
-    },
-    beforeCreate () {
-      getToken()
-      // 如果browserId为空，则新增
-      const browserId = storage({
-        type: 'get',
+export default {
+  name: 'app',
+  data() {
+    return {
+      wid: 'width:1024px'
+    }
+  },
+  beforeCreate() {
+    getToken()
+    if (!window.sessionStorage.getItem('locale')) {
+      window.sessionStorage.setItem('locale', '/cn/')
+    }
+    // 如果browserId为空，则新增
+    const browserId = storage({
+      type: 'get',
+      key: 'browserId'
+    })
+    if (!browserId) {
+      storage({
+        type: 'set',
+        val: guid(),
         key: 'browserId'
       })
-      if (!browserId) {
-        storage({
-          type: 'set',
-          val: guid(),
-          key: 'browserId'
-        })
-      }
-    },
-    async created () {
-      if (document.body.offsetWidth < 1400) {
-        document.body.style.width = '1400px'
-      } else {
-        this.wid = 'width:' + document.body.offsetWidth + 'px'
-      }
-    },
-    methods: {
     }
+  },
+  async created() {
+    if (document.body.offsetWidth < 1400) {
+      document.body.style.width = '1400px'
+    } else {
+      this.wid = 'width:' + document.body.offsetWidth + 'px'
+    }
+  },
+  methods: {
   }
+}
 </script>
 
 <style>
-  @import './assets/styles/colors.less';
-  @import './assets/styles/helpers.less';
-  @import './assets/styles/vars.less';
+@import "./assets/styles/colors.less";
+@import "./assets/styles/helpers.less";
+@import "./assets/styles/vars.less";
 
-  body {
-    padding: 0px;
-    margin: 0px;
-    background-color: #fff;
-    font-family: "微软雅黑";
-  }
+body {
+  padding: 0px;
+  margin: 0px;
+  background-color: #fff;
+  font-family: "微软雅黑";
+}
 
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
+a {
+  text-decoration: none;
+  color: inherit;
+}
 </style>
